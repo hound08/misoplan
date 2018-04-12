a3de826d14c389f172687582032d54dbb5a3862c
bestlove20 <yhoooj@naver.com>
2018-04-12 16:10:17 +0900

    A

--- src/service/myWishListFormAction.java
+++ src/service/myWishListFormAction.java
@@ -1,11 +1,13 @@
 package service;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.AccompanyBoardDto;
 import dao.WishlistDao;
 
 public class myWishListFormAction implements CommandProcess {
@@ -16,7 +18,7 @@ public class myWishListFormAction implements CommandProcess {
 		try {
 			String email = request.getParameter("email");
 			WishlistDao dao = WishlistDao.getInstance();
-			
+			List<WishlistDto> list = dao.select(email);
 			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
