a2319840d7750a3e9d6f2e07491afad59f7aa324
bestlove20 <yhoooj@naver.com>
2018-04-12 17:43:25 +0900

    a

--- src/service/myWishListProAction.java
+++ src/service/myWishListProAction.java
@@ -6,12 +6,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.WishlistDao;
+
 public class myWishListProAction implements CommandProcess {
 
 	@Override
 	public String requestPro(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		
+			String email = request.getParameter("email");
 		try {
 			
 		} catch (Exception e) {
