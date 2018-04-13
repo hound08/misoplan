5dd0c278056a3c90cb909c6f2fc1825ebae9182e
khm7399 <khm7399@gmail.com>
2018-04-13 16:28:36 +0900

    형묵 프로퍼티즈 생성

--- src/service/PlanViewAction.java
+++ src/service/PlanViewAction.java
@@ -22,18 +22,7 @@ public class PlanViewAction implements CommandProcess {
 			
 			request.setAttribute("bs_num", bs_num);
 			request.setAttribute("dto", dto);
-			
-			/*request.setAttribute("sl_code", dto.getSl_code());
-			request.setAttribute("nickname", dto.getNickname());
-			request.setAttribute("title", dto.getTitle());
-			request.setAttribute("content", dto.getContent());
-			request.setAttribute("image_url", dto.getImage_url());
-			request.setAttribute("vote_count", dto.getVote_count());
-			request.setAttribute("view_count", dto.getView_count());
-			request.setAttribute("board_date", dto.getBoard_date());
-			request.setAttribute("schedule_date", dto.getSchedule_date());
-			request.setAttribute("area_name", dto.getArea_name());		
-			request.setAttribute("area_names", dto.getArea_names());	*/
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
