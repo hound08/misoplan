55ec65a62c38733c9293355a957f5c8a0416f70c
khm7399 <khm7399@gmail.com>
2018-04-23 15:24:56 +0900

    형묵 푸시

--- src/dao/mySchduleDao.java
+++ src/dao/mySchduleDao.java
@@ -45,11 +45,8 @@ public class mySchduleDao {
 	      PreparedStatement ps = null;
 	      ResultSet rs = null;
 	      String sql2 = "select l.is_deleted, l.sl_code,l.s_name, m.AREA_NAME, m.sigungu_name, m.TOUR_DATE TOUR_DATE_START, l.REGI_DATE from SCHEDULELARGE l, "
-<<<<<<< HEAD
-	      		+ "schedulemedium m where  l.sl_code = m.sl_code and email = ? and l.is_deleted = 0 order by m.sl_code";
-=======
+	      		+ "schedulemedium m where  l.sl_code = m.sl_code and email = ? and l.is_deleted = 0 order by m.sl_code"
 	      		+ "schedulemedium m where  l.sl_code = m.sl_code and email = ? and l.is_deleted = 0 ORDER BY l.sl_code";
->>>>>>> 571c24dec4ab433680e2f16dc4c2a93cd275e3ee
 	      List<mySchduleDto> planList = new ArrayList<mySchduleDto>();
 	      
 	      try {
